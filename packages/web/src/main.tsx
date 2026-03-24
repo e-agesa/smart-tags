47a8d3ff685550ed8e39b392fe6c770dc6397825
Trevor Twinfusion <agesaeric@gmail.com>
2026-03-24 03:14:50 +0300

    fix: remove unused Navigate import for production build

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
